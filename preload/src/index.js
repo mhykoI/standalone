50ce28e3550a0c9881686ebe578e3e40760f2fd4
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-01-19 05:41:57 +0300

    a

--- preload/src/index.js
+++ preload/src/index.js
@@ -3,8 +3,31 @@ import { patchDevTools } from "./patches/devtools.js";
 import { patchPreload } from "./patches/preload.js";
 import { patchWebpackChunk } from "./patches/webpack-chunk.js";
 
-contextBridge.exposeInMainWorld("require", require);
-contextBridge.exposeInMainWorld("process", process);
+function deepClone(obj, hash = new WeakMap()) {
+  if (Object(obj) !== obj) return obj; // primitives
+  if (hash.has(obj)) return hash.get(obj); // cyclic reference
+  const result = obj instanceof Set ? new Set(obj) // See note about this!
+    : obj instanceof Map ? new Map(Array.from(obj, ([key, val]) =>
+      [key, deepClone(val, hash)]))
+      : obj instanceof Date ? new Date(obj)
+        : obj instanceof RegExp ? new RegExp(obj.source, obj.flags)
+          // ... add here any specific treatment for other classes ...
+          // and finally a catch-all:
+          : obj.constructor ? new obj.constructor()
+            : Object.create(null);
+  hash.set(obj, result);
+  return Object.assign(result, ...Object.keys(obj).map(
+    key => ({ [key]: deepClone(obj[key], hash) })));
+}
+
+contextBridge.exposeInMainWorld("acord", {
+  modules: {
+    require
+  },
+  internal: {
+    process
+  }
+});
 
 patchWebpackChunk();
 patchDevTools();
