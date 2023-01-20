f5efcbaa665098e62b044207c6c867f2ede3ed7c
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-01-20 20:21:27 +0300

    a

--- renderer/src/api/index.js
+++ renderer/src/api/index.js
@@ -1,6 +1,7 @@
 import modules from './modules';
 import dev from './dev';
 import utils from './utils';
+import extensions from './extensions';
 
 utils.logger.debug(`PRELOAD_KEY: <PRELOAD_KEY>`);
 
@@ -11,6 +12,10 @@ export default {
     get modules() {
       if (!dev.enabled) throw new Error("Modules API can only be accessed when Dev mode is enabled!");
       return modules;
+    },
+    get extensions() {
+      if (!dev.enabled) throw new Error("Extensions API can only be accessed when Dev mode is enabled!");
+      return extensions;
     },
     get internal() {
       if (!dev.enabled) throw new Error("Internal API can only be accessed when Dev mode is enabled!");
@@ -24,6 +29,7 @@ export default {
     dev,
     modules,
     utils,
+    extensions,
     internal: {
       process: globalThis["<PRELOAD_KEY>"].process,
       isDevToolsOpen: globalThis["<PRELOAD_KEY>"].isDevToolsOpen,
