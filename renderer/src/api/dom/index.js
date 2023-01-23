9ac9f842529a67148788934195a35f2b28f42141
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-01-24 02:36:41 +0300

    dom apis

--- renderer/src/api/dom/index.js
+++ renderer/src/api/dom/index.js
@@ -5,7 +5,54 @@ export default {
     const div = document.createElement('div');
     div.innerHTML = html;
     return div.firstElementChild;
-  }
+  },
+  toCSSProp(o) {
+    let elm = document.createElement("div");
+    Object.entries(o).forEach((i) => {
+      if (elm.style.hasOwnProperty(i[0])) {
+        elm.style[i[0]] = i[1];
+      } else {
+        elm.style.setProperty(i[0], i[1]);
+      }
+    });
+    return elm.getAttribute("style");
+  },
+  toHTMLProps(o) {
+    return Object.entries(o)
+      .map(
+        (i) =>
+          `${i[0].replace(/ +/, "-")}="${i[0] == "style" && typeof i[1] != "string"
+            ? this.toCSSProp(i[1])
+            : this.escapeHTML(i[1])}"`
+      )
+      .join(" ");
+  },
+  escape(html) {
+    return new Option(html).innerHTML;
+  },
+  /**
+   * @param {Element} elm 
+   * @param {number|string} selectorOrNumber 
+   * @returns {Element[]}
+   */
+  parents(elm, selectorOrNumber) {
+    let parents = [];
+    if (typeof selectorOrNumber === "number") {
+      for (let i = 0; i < selectorOrNumber; i++) {
+        if (elm.parentElement) {
+          elm = elm.parentElement;
+          parents.push(elm);
+        }
+      }
+    } else {
+      while (elm.parentElement && elm.parentElement.closest(selectorOrNumber)) {
+        elm = elm.parentElement.closest(selectorOrNumber);
+        parents.push(elm);
+      }
+    }
+    return parents;
+  },
+  // TODO: add patch api
 }
 
 {
