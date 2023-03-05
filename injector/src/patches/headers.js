48bedfc06c700323a5a10edceccf2472620ed29f
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-05 17:30:28 +0300

    fix youtube embeds

--- injector/src/patches/headers.js
+++ injector/src/patches/headers.js
@@ -1,8 +1,10 @@
 import electron from "electron";
 
 export function patchHeaders() {
-  const keysToDelete = ["content-security-policy", "access-control-allow-origin"];
+  const keysToDelete = ["content-security-policy", "access-control-allow-origin", "access-control-allow-credentials", "access-control-expose-headers"];
+  const ignoreList = ["googlevideo.com"];
   electron.session.defaultSession.webRequest.onHeadersReceived((details, cb) => {
+    if (ignoreList.some((x) => details.url.includes(x))) return cb({ cancel: false, responseHeaders: details.responseHeaders });
     Object.keys(details.responseHeaders).forEach((key) => {
       if (!keysToDelete.includes(key.toLowerCase())) return;
       delete details.responseHeaders[key];
