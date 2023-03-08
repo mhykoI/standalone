8233241194821b3681ba8cc1e694c5469b954747
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-08 23:09:27 +0300

    more strict cors

--- injector/src/patches/headers.js
+++ injector/src/patches/headers.js
@@ -2,9 +2,9 @@ import electron from "electron";
 
 export function patchHeaders() {
   const keysToDelete = ["content-security-policy", "access-control-allow-origin", "access-control-allow-credentials", "access-control-expose-headers"];
-  const ignoreList = ["googlevideo.com", "discord.com/assets"];
+  const allowList = ["cdn.discordapp.com", "discord.com/channels"];
   electron.session.defaultSession.webRequest.onHeadersReceived((details, cb) => {
-    if (ignoreList.some((x) => details.url.includes(x))) return cb({ cancel: false, responseHeaders: details.responseHeaders });
+    if (!allowList.some(i => details.url.toLowerCase().includes(i))) return cb({ cancel: false, responseHeaders: details.responseHeaders });
     Object.keys(details.responseHeaders).forEach((key) => {
       if (!keysToDelete.includes(key.toLowerCase())) return;
       delete details.responseHeaders[key];
