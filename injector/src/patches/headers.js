953f0a39c36d270a1354a6efd4bd4fae7c7534ba
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-08 23:20:04 +0300

    cors stuff

--- injector/src/patches/headers.js
+++ injector/src/patches/headers.js
@@ -1,12 +1,15 @@
 import electron from "electron";
 
 export function patchHeaders() {
-  const keysToDelete = ["content-security-policy", "access-control-allow-origin", "access-control-allow-credentials", "access-control-expose-headers"];
-  const allowList = ["cdn.discordapp.com", "discord.com/channels"];
+  const keysToDelete = [
+    "content-security-policy",
+    "access-control-allow-origin"
+  ];
+  const ignoreList = ["googlevideo.com"];
   electron.session.defaultSession.webRequest.onHeadersReceived((details, cb) => {
-    if (!allowList.some(i => details.url.toLowerCase().includes(i))) return cb({ cancel: false, responseHeaders: details.responseHeaders });
+    if (ignoreList.some((x) => details.url.includes(x))) return cb({ cancel: false, responseHeaders: details.responseHeaders });
     Object.keys(details.responseHeaders).forEach((key) => {
-      if (!keysToDelete.includes(key.toLowerCase())) return;
+      if (!(keysToDelete.includes(key.toLowerCase()))) return;
       delete details.responseHeaders[key];
     });
     details.responseHeaders["Access-Control-Allow-Origin"] = ["*"];
