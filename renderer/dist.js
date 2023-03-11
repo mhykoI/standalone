e44d21cfcbba65a92d79c26de9b2da77988efd72
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-11 16:45:33 +0300

    fixes

--- renderer/dist.js
+++ renderer/dist.js
@@ -3364,23 +3364,24 @@
     load(vueApp) {
       vueApp.component("discord-select", {
         template: `
-        <div class="acord--discord-select acord--discord-select-lookFilled acord--discord-select" :class="{'acord--discord-select-open': active}">
-          <div class="acord--discord-select-value">{{options.find(i=>i.value === modelValue)?.label}}</div>
-          <div class="acord--discord-select-icons">
+        <div class="\`acord--discord-select acord--discord-select-lookFilled acord--discord-select \${id}\`" :class="{'acord--discord-select-open': active}">
+          <div class="\`acord--discord-select-value \${id}\`">{{options.find(i=>i.value === modelValue)?.label}}</div>
+          <div class="\`acord--discord-select-icons \${id}\`">
               <svg v-if="!active" class="icon" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M16.59 8.59003L12 13.17L7.41 8.59003L6 10L12 16L18 10L16.59 8.59003Z"></path></svg>
-              <svg v-else class="icon" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 16.0001L12 11.4201L16.59 16.0001L18 14.5901L12 8.59006L6 14.5901L7.41 16.0001Z"></path></svg>
+              <svg v-else class="\`icon \${id}\`" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 16.0001L12 11.4201L16.59 16.0001L18 14.5901L12 8.59006L6 14.5901L7.41 16.0001Z"></path></svg>
           </div>
-          <div v-if="active" class="options acord--discord-select-popout acord--discord-select-scrollerBase acord--discord-select-scrollThin" :class="{'top-popout': popoutPosition === 'top'}">
-            <div v-for="option in options" class="option acord--discord-select-option" @click="onOptionClick($event, option)" :key="option.value" :aria-selected="\`\${modelValue === option.value}\`">
+          <div v-if="active" class="\`options acord--discord-select-popout acord--discord-select-scrollerBase acord--discord-select-scrollThin \${id}\`" :class="{'top-popout': popoutPosition === 'top'}">
+            <div v-for="option in options" class="\`option acord--discord-select-option \${id}\`" @click="onOptionClick($event, option)" :key="option.value" :aria-selected="\`\${modelValue === option.value}\`">
               {{option.label}}
-              <svg v-if="modelValue === option.value" class="acord--discord-select-selectedIcon" role="img" width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle r="8" cx="12" cy="12" fill="white"></circle><g fill="none" fill-rule="evenodd"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path></g></svg>
+              <svg v-if="modelValue === option.value" class="\`acord--discord-select-selectedIcon \${id}\`" role="img" width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle r="8" cx="12" cy="12" fill="white"></circle><g fill="none" fill-rule="evenodd"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path></g></svg>
             </div>
           </div>
         </div>
       `,
         data() {
           return {
-            active: false
+            active: false,
+            id: Math.random().toString(36).substr(2, 9)
           };
         },
         props: ["options", "modelValue", "popoutPosition"],
@@ -3397,7 +3398,7 @@
             this.$emit("change", { value: option.value, event });
           },
           onClick(e) {
-            if (e.target.classList.contains("acord--discord-select") || e.target.classList.contains("acord--discord-select-value") || e.target.classList.contains("acord--discord-select-icons") || e.target.classList.contains("acord--discord-select-popout") || e.target.classList.contains("acord--discord-select-option") || e.target.classList.contains("icon")) {
+            if (e.target.classList.contains(this.id)) {
               this.active = !this.active;
               return;
             }
@@ -4787,8 +4788,8 @@
           template: `
           <div class="acord--installed-extension-card" @contextmenu="onContextMenu">
             <div class="status-container">
-              <div class="loaded-state" :class="{'active': !!this.configCache}" :acord--tooltip-content="i18nFormat(!!this.configCache ? 'EXTENSION_ACTIVE' : 'EXTENSION_INACTIVE')"></div>
-              <div v-if="extension.manifest.mode == 'development'" class="development-mode-warning" :acord--tooltip-content="i18nFormat('EXTENSION_IS_IN_DEVELOPMENT_MODE')">
+              <div class="loaded-state" :class="{'active': !!this.configCache}" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat(!!this.configCache ? 'EXTENSION_ACTIVE' : 'EXTENSION_INACTIVE')"></div>
+              <div v-if="extension.manifest.mode == 'development'" class="development-mode-warning" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat('EXTENSION_IS_IN_DEVELOPMENT_MODE')">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
                   <path fill="currentColor" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-7v2h2v-2h-2zm0-8v6h2V7h-2z"/>
                 </svg>
@@ -4818,7 +4819,7 @@
               <div class="right">
                 <div class="top">
                   <div v-if="!hideControls" class="controls">
-                    <div class="control" @click="onUpdateExtension" :acord--tooltip-content="i18nFormat('UPDATE_EXTENSION')">
+                    <div class="control" @click="onUpdateExtension" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat('UPDATE_EXTENSION')">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                         <path fill="currentColor" d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z"/>
                       </svg>
@@ -4828,7 +4829,7 @@
                         <path fill="currentColor" d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z"/>
                       </svg>
                     </div>
-                    <div class="control" @click="onToggleExtension" :acord--tooltip-content="i18nFormat(enabled ? 'DISABLE_EXTENSION' : 'ENABLE_EXTENSION')">
+                    <div class="control" @click="onToggleExtension" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat(enabled ? 'DISABLE_EXTENSION' : 'ENABLE_EXTENSION')">
                       <svg v-if="!enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                         <path fill="currentColor" d="M8 7a5 5 0 1 0 0 10h8a5 5 0 0 0 0-10H8zm0-2h8a7 7 0 0 1 0 14H8A7 7 0 0 1 8 5zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                       </svg>
--- renderer/src/ui/home/vue/components/components/cards/installed-extension-card/index.js
+++ renderer/src/ui/home/vue/components/components/cards/installed-extension-card/index.js
@@ -17,8 +17,8 @@ export default {
         template: `
           <div class="acord--installed-extension-card" @contextmenu="onContextMenu">
             <div class="status-container">
-              <div class="loaded-state" :class="{'active': !!this.configCache}" :acord--tooltip-content="i18nFormat(!!this.configCache ? 'EXTENSION_ACTIVE' : 'EXTENSION_INACTIVE')"></div>
-              <div v-if="extension.manifest.mode == 'development'" class="development-mode-warning" :acord--tooltip-content="i18nFormat('EXTENSION_IS_IN_DEVELOPMENT_MODE')">
+              <div class="loaded-state" :class="{'active': !!this.configCache}" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat(!!this.configCache ? 'EXTENSION_ACTIVE' : 'EXTENSION_INACTIVE')"></div>
+              <div v-if="extension.manifest.mode == 'development'" class="development-mode-warning" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat('EXTENSION_IS_IN_DEVELOPMENT_MODE')">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
                   <path fill="currentColor" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-7v2h2v-2h-2zm0-8v6h2V7h-2z"/>
                 </svg>
@@ -48,7 +48,7 @@ export default {
               <div class="right">
                 <div class="top">
                   <div v-if="!hideControls" class="controls">
-                    <div class="control" @click="onUpdateExtension" :acord--tooltip-content="i18nFormat('UPDATE_EXTENSION')">
+                    <div class="control" @click="onUpdateExtension" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat('UPDATE_EXTENSION')">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                         <path fill="currentColor" d="M5.463 4.433A9.961 9.961 0 0 1 12 2c5.523 0 10 4.477 10 10 0 2.136-.67 4.116-1.81 5.74L17 12h3A8 8 0 0 0 6.46 6.228l-.997-1.795zm13.074 15.134A9.961 9.961 0 0 1 12 22C6.477 22 2 17.523 2 12c0-2.136.67-4.116 1.81-5.74L7 12H4a8 8 0 0 0 13.54 5.772l.997 1.795z"/>
                       </svg>
@@ -58,7 +58,7 @@ export default {
                         <path fill="currentColor" d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z"/>
                       </svg>
                     </div>
-                    <div class="control" @click="onToggleExtension" :acord--tooltip-content="i18nFormat(enabled ? 'DISABLE_EXTENSION' : 'ENABLE_EXTENSION')">
+                    <div class="control" @click="onToggleExtension" acord--tooltip-ignore-destroy :acord--tooltip-content="i18nFormat(enabled ? 'DISABLE_EXTENSION' : 'ENABLE_EXTENSION')">
                       <svg v-if="!enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
                         <path fill="currentColor" d="M8 7a5 5 0 1 0 0 10h8a5 5 0 0 0 0-10H8zm0-2h8a7 7 0 0 1 0 14H8A7 7 0 0 1 8 5zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                       </svg>
