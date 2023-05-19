ff3191d5105726c3498d91255db9514f60876929
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-05-19 16:10:16 +0300

    yeah

--- renderer/src/ui/home/vue/components/components/cards/inventory/inventory-colored-name-feature-card/index.js
+++ renderer/src/ui/home/vue/components/components/cards/inventory/inventory-colored-name-feature-card/index.js
@@ -57,7 +57,7 @@ export default {
                   <div class="color" v-for="(point, idx) in points" :key="idx">
                     <input v-model="point.color" type="color" class="color-input" />
                     <input v-model="point.percentage" type="number" step="0.05" class="percentage-input" max="100" min="0" />
-                    <div class="remove" @click="removeColor(idx)">
+                    <div class="remove" :class="{disabled: points.length <= 1}" @click="removeColor(idx)">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M12.0007 10.5865L16.9504 5.63672L18.3646 7.05093L13.4149 12.0007L18.3646 16.9504L16.9504 18.3646L12.0007 13.4149L7.05093 18.3646L5.63672 16.9504L10.5865 12.0007L5.63672 7.05093L7.05093 5.63672L12.0007 10.5865Z"></path>
                       </svg>
